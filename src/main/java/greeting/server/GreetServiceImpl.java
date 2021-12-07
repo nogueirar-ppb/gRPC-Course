0b36677c81685df163479fef9aea52c59858615b
nogueirar <ricardo.nogueira@paddypowerbetfair.com>
2021-12-07 16:28:58 +0000

    imlpemented bidi server

--- src/main/java/greeting/server/GreetServiceImpl.java
+++ src/main/java/greeting/server/GreetServiceImpl.java
@@ -82,4 +82,31 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
         };
         return requestObserver;
     }
+
+    @Override
+    public StreamObserver<GreetEveryoneRequest> greetEveryone(StreamObserver<GreetEveryoneResponse> responseObserver) {
+        StreamObserver<GreetEveryoneRequest> requestObserver = new StreamObserver<GreetEveryoneRequest>() {
+            @Override
+            public void onNext(GreetEveryoneRequest value) {
+                String response = "Hello " + value.getGreeting().getFirstName();
+                GreetEveryoneResponse greetEveryoneResponse = GreetEveryoneResponse.newBuilder()
+                        .setResponse(response)
+                        .build();
+
+                responseObserver.onNext(greetEveryoneResponse);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                //do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestObserver;
+    }
 }
